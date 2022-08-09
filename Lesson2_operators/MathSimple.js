2a02c3f93e18067aff1d36b5cb2b8894df9bbd13
Vladimir Stroganov <vlastroganov@gmail.com>
2022-08-09 16:45:37 +0300

    Арифметические операции

---
+++ Lesson2_operators/MathSimple.js
@@ -0,0 +1,20 @@
+function Multiply(a, b) {
+    return a * b;
+}
+function Divide(a, b) {
+    return a / b;
+}
+function Addition(a, b) {
+    return a + b;
+}
+function Subtraction(a, b) {
+    return a - b;
+}
+let z = Multiply(4, 8);
+alert(z);
+z = Divide(4, 8);
+alert(z);
+z = Addition(4, 8);
+alert(z);
+z = Subtraction(4, 8);
+alert(z);
