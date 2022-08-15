6c270cb6b6561501916898991b132ca9fddb5398
Vladimir Stroganov <vlastroganov@gmail.com>
2022-08-15 17:47:56 +0300

    Цикл четных и нечетных чисел

---
+++ Lesson3_LoopsArraysStructs/Task1.js
@@ -0,0 +1,10 @@
+"use strict";
+for (var i = 0; i < 11; i++) {
+    if (i == 0) {
+        console.log(`${i} - это ноль`)
+    }
+    else if (i % 2 == 0) {
+        console.log(`${i} - четное число`)
+    }
+    else { console.log(`${i} - нечетное число`) }
+}
