76d2b051348535fec6660dc4e56e45b3310b332e
Vladimir Stroganov <vlastroganov@gmail.com>
2022-08-15 22:00:33 +0300

    Наследование в стиле es5

---
+++ Lesson4_Objects/Task1-2.js
@@ -0,0 +1,23 @@
+"use strict";
+function Post(author, text, date) {
+    this.author = author;
+    this.text = text;
+    this.date = date;
+}
+Post.prototype.edit = function (text) {
+    this.text = text;
+}
+function AttachedPost(author, text, date) {
+    Post.call(this, author, text, date);
+    this.highlighted = false;
+}
+AttachedPost.prototype = Object.create(Post.prototype);
+AttachedPost.prototype.constructor = AttachedPost;
+
+AttachedPost.prototype.makeTextHighlighted = function () {
+    this.highlighted = true;
+};
+
+let aPost = new AttachedPost("Genry", "MegaText", 1991);
+aPost.makeTextHighlighted();
+let t = 0;
