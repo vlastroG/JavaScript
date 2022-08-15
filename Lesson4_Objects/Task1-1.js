bcf50c106879c584f349a0b1ccbefd79754d4b6f
Vladimir Stroganov <vlastroganov@gmail.com>
2022-08-15 21:37:28 +0300

    Тип в стиле es6

--- Lesson4_Objects/Task1-1.js
+++ Lesson4_Objects/Task1-1.js
@@ -1,10 +1,20 @@
 "use strict";
-function Product(name, price) {
-    this.name = name;
-    this.price = price;
+// function Product(name, price) {
+//     this.name = name;
+//     this.price = price;
+// }
+// Product.prototype.make25PercentDiscount = function () {
+//     this.price *= 0.75;
+// };
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    make25PercentDiscount() {
+        this.price *= 0.75;
+    }
 }
-Product.prototype.make25PercentDiscount = function () {
-    this.price *= 0.75;
-};
 let product = new Product("BigOne", 1000);
 product.make25PercentDiscount();
