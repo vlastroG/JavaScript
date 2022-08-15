f20beaabb4b30dedcf00ac999d5a27388c99bf84
Vladimir Stroganov <vlastroganov@gmail.com>
2022-08-15 22:10:06 +0300

    Наследование в стиле es6

--- Lesson4_Objects/Task1-2.js
+++ Lesson4_Objects/Task1-2.js
@@ -1,23 +1,45 @@
 "use strict";
-function Post(author, text, date) {
-    this.author = author;
-    this.text = text;
-    this.date = date;
-}
-Post.prototype.edit = function (text) {
-    this.text = text;
-};
-function AttachedPost(author, text, date) {
-    Post.call(this, author, text, date);
-    this.highlighted = false;
-}
-AttachedPost.prototype = Object.create(Post.prototype);
-AttachedPost.prototype.constructor = AttachedPost;
+// function Post(author, text, date) {
+//     this.author = author;
+//     this.text = text;
+//     this.date = date;
+// }
+// Post.prototype.edit = function (text) {
+//     this.text = text;
+// };
+// function AttachedPost(author, text, date) {
+//     Post.call(this, author, text, date);
+//     this.highlighted = false;
+// }
+// AttachedPost.prototype = Object.create(Post.prototype);
+// AttachedPost.prototype.constructor = AttachedPost;
 
-AttachedPost.prototype.makeTextHighlighted = function () {
-    this.highlighted = true;
-};
+// AttachedPost.prototype.makeTextHighlighted = function () {
+//     this.highlighted = true;
+// };
 
+class Post {
+    constructor(author, text, date) {
+        this.author = author;
+        this.text = text;
+        this.date = date;
+    }
+
+    edit(text) {
+        this.text = text;
+    }
+}
+
+class AttachedPost extends Post {
+    constructor(author, text, date) {
+        super(author, text, date);
+        this.highlighted = false;
+    }
+
+    makeTextHighlighted() {
+        this.highlighted = true;
+    }
+}
 
 let aPost = new AttachedPost("Genry", "MegaText", 1991);
 aPost.makeTextHighlighted();
